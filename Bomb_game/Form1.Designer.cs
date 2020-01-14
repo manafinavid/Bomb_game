164c6afeba234baba72df9714e1a857835c6fed1
manafinavid <manafinavid@yahoo.com>
2020-01-13 20:34:56 -0800

    Initial commit

---
+++ Bomb_game/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Bomb_game
+{
+    partial class Bomb_Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "&New Game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(220, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Source : 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(136, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "00:00";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(2, 44);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(301, 237);
+            this.panel1.TabIndex = 4;
+            // 
+            // Bomb_Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 298);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Bomb_Game";
+            this.Text = "Bomb Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Bomb_game/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Bomb_game
+{
+    public partial class Bomb_Game : Form
+    {
+        int source = 0;
+        public static int R = 9;
+        public Bomb_Game()
+        {
+            InitializeComponent();
+        }
+        List<List<Button>> buttons = new List<List<Button>>();
+        int NumberOfBomb = 20;
+        int[] bombs;
+        //10, 60
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tur();
+        }
+        void tur()
+        {
+            bombs = new int[NumberOfBomb];
+            for (int H = 0; H < R; H++)
+            {
+                List<Button> B = new List<Button>();
+                for (int u = 0; u < R; u++)
+                {
+                    B.Add(new Button());
+                }
+                for (int u = 0; u < R; u++)
+                {
+                    B[u].Size = new System.Drawing.Size(25, 25);
+                    B[u].Click += Buttons_click;
+                }
+                buttons.Add(B);
+            }
+        }
+        void Buttons_click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                if (((Button)sender).Tag == "B")
+                {
+                    for (int y = 0; y < NumberOfBomb; y++)
+                    {
+                        buttons[bombs[y] / R][bombs[y] % R].BackgroundImage = Bomb_game.Properties.Resources.bomb;
+                        buttons[bombs[y] / R][bombs[y] % R].BackgroundImageLayout = ImageLayout.Stretch;
+                    }
+                    timer1.Enabled = false;
+                    Application.DoEvents();
+                    MessageBox.Show("BOMB!!!");
+                }
+                else if (((Button)sender).BackgroundImage == null)
+                {
+                    ((Button)sender).BackgroundImage = Bomb_game.Properties.Resources.blank;
+                    source++;
+                    label1.Text = "Source :" + source;
+                }
+                if (source == R * R - NumberOfBomb)
+                {
+                    MessageBox.Show("You are win!!!");
+                    for (int y = 0; y < NumberOfBomb; y++)
+                    {
+                        buttons[bombs[y] / R][bombs[y] % R].BackgroundImage = Bomb_game.Properties.Resources.bomb;
+                        buttons[bombs[y] / R][bombs[y] % R].BackgroundImageLayout = ImageLayout.Stretch;
+                    }
+                    timer1.Enabled = false;
+                    Application.DoEvents();
+                }
+            }
+        }
+        void new_game()
+        {
+            source = 0;
+            label1.Text= "Source :" + source;
+            Random Rn = new Random();
+            int I = 0, p, o;
+            bombs = new int[NumberOfBomb];
+            for (int i = 0; i < NumberOfBomb; i++)
+            {
+
+                I = Rn.Next(1, R * R);
+                p = I / R;
+                o = I % R;
+                if (buttons[p][o].Tag == "B")
+                {
+                    i--;
+                    continue;
+                }
+                bombs[i] = I;
+                buttons[p][o].Tag = "B";
+            }
+        }
+        void show_buttons()
+        {
+            int this_x = 40, this_y = 0;
+            for (int i = 0; i < R; i++)
+            {
+                for (int j = 0; j < R; j++)
+                {
+                    buttons[i][j].Location = new System.Drawing.Point(this_x, this_y);
+                    this_x += 25;
+                    panel1.Controls.Add(buttons[i][j]);
+                }
+                this_y += 25;
+                this_x -= R * 25;
+            }
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            time_ = 240;
+            panel1.Controls.Clear();
+            buttons.Clear();
+            tur();
+            new_game();
+            show_buttons();
+            timer1.Enabled = true;
+        }
+        int time_ = 240;
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            time_--;
+            if (time_ >= 0)
+            {
+                label2.Text = time_ / 60 + ":" + time_ % 60;
+                return;
+            }
+            for (int y = 0; y < NumberOfBomb; y++)
+            {
+                buttons[bombs[y] / R][bombs[y] % R].BackgroundImage = Bomb_game.Properties.Resources.bomb;
+                buttons[bombs[y] / R][bombs[y] % R].BackgroundImageLayout = ImageLayout.Stretch;
+            }
+            timer1.Enabled = false;
+            Application.DoEvents();
+            MessageBox.Show("BOMB!!!");
+        }
+    }
+}
